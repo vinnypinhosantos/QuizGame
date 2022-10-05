b91b7222f215d4b22be4d35de3b71bc947797c21
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-10-04 23:20:11 -0300

    Creating the QuizGame

---
+++ data.py
@@ -0,0 +1,35 @@
+question_data = [{"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004 'The Aviator'",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of "
+                              "the movies Interstellar and The Martian.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "The word 'Inception' came from the 2010 blockbuster hit 'Inception'.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "Ewan McGregor did not know the name of the second prequel film of Star Wars during "
+                              "and after filming.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "Samuel L. Jackson had the words, Bad Motherf*cker in-scripted on his"
+                              " lightsaber during the filming of Star Wars.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "In Alfred Hitchcock's film 'Psycho' it is said he used chocolate "
+                              "syrup to simulate the blood in the famous shower scene from ",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "In the movie The Revenant, DiCaprio's character hunts down the killer of his son.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "Brandon Routh plays the titular character in the movie 'John Wick'.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "'Minions' was released on the June 10th, 2015.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+                  "question": "'Foodfight' earned less than $80,000 at box office.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]}
+]
